019303a2af25a642895e0c1e801664ccaf53dcfd
Frank Hui <huifrank@live.com>
2020-05-16 22:09:52 -0700

    Add half ass animation

--- js/main.js
+++ js/main.js
@@ -9,11 +9,9 @@ $(document).ready(function(){
     offset = startchange.offset();
 });
 
+
 $(document).scroll(function() {
     scroll_start = $(this).scrollTop();
-    console.log(scroll_start)
-    console.log(offset.top)
-    console.log(pastStartChange)
     if(scroll_start > offset.top && !pastStartChange) {
         anime({
             targets:'.navbar',
@@ -49,3 +47,116 @@ $(document).on('click', 'a[href^="#"]', function(e) {
     // animated top scrolling
     $('body, html').animate({scrollTop: pos});
 });
+
+function transparentChildren(node) {
+    children = node.childNodes
+    for (var i = 0; i < children.length; i++) {
+        try {
+            anime({
+                targets: node.childNodes[i],
+                opacity: 0
+            })
+        } catch (e) {
+
+        }
+    }
+}
+
+function untransparentChildren(node) {
+    children = node.childNodes
+
+    for (var i = 0; i < children.length; i++) {
+        try {
+            anime({
+                targets: node.childNodes[i],
+                opacity: 1
+            })
+        } catch (e) {
+
+        }
+    }
+}
+
+$('.qualifications').hover(function () {
+        c1 = this.parentNode.parentNode.childNodes
+        c2 = []
+        c3 = []
+
+        for (child in c1) {
+            try {
+                if (c1[child].childNodes && c1[child].classList.contains("col")) {
+                    c2.push(c1[child])
+                }
+            } catch (e) {
+
+            }
+        }
+        for (child in c2) {
+            try {
+                if (c2[child].childNodes.length > 0) {
+                    c3.push(c2[child].childNodes[1])
+                }
+            } catch (e) {
+
+            }
+        }
+
+        for (var i = 0; i < c3.length; i++) {
+            transparentChildren(c3[i])
+        }
+
+        object = this
+
+        c3 = c3.filter(function (value, index, arr) {
+            console.log(object.classList.toString())
+            console.log(value.classList.toString())
+            return object.classList.toString() !== value.classList.toString()
+        })
+        console.log(c3)
+        anime({
+            targets: this,
+            width: '150%'
+        })
+
+        for (child in c3) {
+            anime({
+                targets: c3,
+                translateX: 100,
+                width: '50%'
+            })
+        }
+    },
+    function () {
+        c1 = this.parentNode.parentNode.childNodes
+        c2 = []
+        c3 = []
+
+        for (child in c1) {
+            try {
+                if (c1[child].childNodes && c1[child].classList.contains("col")) {
+                    c2.push(c1[child])
+                }
+            } catch (e) {
+
+            }
+        }
+        for (child in c2) {
+            try {
+                if (c2[child].childNodes.length > 0) {
+                    c3.push(c2[child].childNodes[1])
+                }
+            } catch (e) {
+
+            }
+        }
+
+        for (var i = 0; i < c3.length; i++) {
+            untransparentChildren(c3[i])
+        }
+
+        anime({
+            targets: c3,
+            translateX: 0,
+            width: '100%'
+        })
+    });
